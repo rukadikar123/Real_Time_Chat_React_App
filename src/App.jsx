e3311580bc9a378aa9554acf271af14d4a878bf9
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-01-30 16:56:20 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
@@ -6,7 +7,12 @@ function App() {
 
   return (
     <>
-     
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={</>}/>
+            <Route path="/homepage" element={</>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
