deaf064b558aca6b47ce61788f2c64d2c06becea
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-02-02 18:59:27 +0530

    fix bug

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app); // Create an HTTP server using the Expres
 // Initialize a new Socket.io server attached to the HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "https://679f1ec16dc670ba9bf5a161--peppy-cobbler-174f4a.netlify.app/", // The URL of the front-end
+    origin:"https://679f1ec16dc670ba9bf5a161--peppy-cobbler-174f4a.netlify.app", // The URL of the front-end
     methods: ["GET", "POST"], // HTTP methods allowed for the communication
   },
 });
